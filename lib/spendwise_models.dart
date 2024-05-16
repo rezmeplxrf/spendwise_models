f2ffa899ca2f18f173197ad7eb19467046c1e0aa
Donghu <rezmeplx@rfway.org>
2024-05-17 02:02:44 +0900

    d

--- lib/spendwise_models.dart
+++ lib/spendwise_models.dart
@@ -5,3 +5,4 @@ export '/src/product/product.dart';
 export '/src/platform/platforms.dart';
 export '/src/hotdeal/hotdeal.dart';
 export '/src/etc/mylogs.dart';
+export '/src/detail/detail_param.dart';
